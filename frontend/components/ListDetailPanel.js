0b597439c631389664b86d3e6e817cc42af57d6c
BugMaker <81194561+JorgeJFCovello@users.noreply.github.com>
2022-10-07 12:57:41 -0300

    add new task dialog

--- frontend/components/ListDetailPanel.js
+++ frontend/components/ListDetailPanel.js
@@ -2,7 +2,19 @@ import { DataGrid } from '@mui/x-data-grid';
 import { useState } from 'react';
 import * as React from 'react';
 import Router from 'next/router';
-import { Button, Card, CardContent, Checkbox, Grid } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  TextField,
+} from '@mui/material';
 import moment from 'moment';
 
 const columns = [
@@ -63,7 +75,14 @@ export default function ListPanel(props) {
   const { listName } = props;
   const [tasks, setTasks] = useState(rows);
   const [pageSize, setPageSize] = useState(5);
+  const [openCreationDialog, setOpenCreationDialog] = useState(false);
+  const [newTaskName, setNewTaskName] = useState('');
   //useEffect para sacar las listas de un websocket
+
+  const saveNewTask = () => {
+    console.log('saving new task', newTaskName);
+    setOpenCreationDialog(false);
+  };
   return (
     <Grid container justifyContent="center">
       <Grid item xs={8}>
@@ -79,8 +98,39 @@ export default function ListPanel(props) {
               rowsPerPageOptions={[5, 10, 25, 50]}
             />
           </CardContent>
+          <CardActions>
+            <Button onClick={() => setOpenCreationDialog(true)}>
+              Add new task
+            </Button>
+          </CardActions>
         </Card>
       </Grid>
+      <Dialog open={openCreationDialog}>
+        <DialogTitle>Create new task</DialogTitle>
+        <DialogContent>
+          <Grid container justifyContent="center">
+            <Grid item xs={8}>
+              <TextField
+                value={newTaskName}
+                onChange={(e) => setNewTaskName(e.target.value)}
+                label="task Name"
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => setOpenCreationDialog(false)}
+          >
+            Cancel
+          </Button>
+          <Button variant="outlined" onClick={saveNewTask}>
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 }
