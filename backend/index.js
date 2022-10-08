f6f90e2c2ea6a8cb5aee145343d0adf58a715626
BugMaker <81194561+JorgeJFCovello@users.noreply.github.com>
2022-10-08 19:03:00 -0300

    add redis

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const { createClient } = require('redis');
 const socketCache = require('./utils/sockets');
 const app = express();
 const port = 8080;
@@ -17,6 +18,19 @@ io.on('connection', (socket) => {
   connectionData = new Date().getTime().toString();
   socketCache[connectionData] = socket;
 });
+
+const client = createClient({ url: 'redis://default:redispw@localhost:49153' });
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+const initRedis = () =>
+  client
+    .connect()
+    .then(() => console.log('Connected to redis'))
+    .catch((err) => {
+      console.log('Error conecting redis', err);
+      setTimeout(initRedis, 60000);
+    });
+initRedis();
 app.use(express.json());
 app.use('/api', routes);
 http.listen(port, () => console.log(`App listening on port ${port}!`));
