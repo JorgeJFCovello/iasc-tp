f0f14c211838dc8c9207c340fece29a5dd80604c
Jorge Covello <81194561+JorgeJFCovello@users.noreply.github.com>
2022-11-29 22:53:21 -0300

    Update build-k8-images.sh

--- build-k8-images.sh
+++ build-k8-images.sh
@@ -1,3 +1,6 @@
 docker build -t jferc1310/iasc-k8s-frontend:latest ./frontend
 docker build -t jferc1310/iasc-k8s-backend:latest ./backend
 docker build -t jferc1310/iasc-k8s-proxy:latest ./proxy
+docker push jferc1310/iasc-k8s-frontend:latest
+docker push jferc1310/iasc-k8s-backend:latest 
+docker push jferc1310/iasc-k8s-proxy:latest 
