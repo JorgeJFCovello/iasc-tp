9d9851a983f26fefb01c22142a99aba952e48c1d
Jorge Covello <81194561+JorgeJFCovello@users.noreply.github.com>
2022-12-08 10:47:23 -0300

    add k8

--- build-k8-images.sh
+++ build-k8-images.sh
@@ -1,6 +1,6 @@
-docker build -t jferc1310/iasc-k8s-frontend:latest ./frontend
-docker build -t jferc1310/iasc-k8s-backend:latest ./backend
-docker build -t jferc1310/iasc-k8s-proxy:latest ./proxy
-docker push jferc1310/iasc-k8s-frontend:latest
-docker push jferc1310/iasc-k8s-backend:latest 
-docker push jferc1310/iasc-k8s-proxy:latest 
+docker build -t jferc1310/iasc-k8s-frontend:demo ./frontend
+docker build -t jferc1310/iasc-k8s-backend:demo ./backend
+docker build -t jferc1310/iasc-k8s-proxy:demo ./proxy
+docker push jferc1310/iasc-k8s-frontend:demo
+docker push jferc1310/iasc-k8s-backend:demo 
+docker push jferc1310/iasc-k8s-proxy:demo 
